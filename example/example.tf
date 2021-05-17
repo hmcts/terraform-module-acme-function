c1fde807977aa22cac76ef00b4645f9e2ce7ff01
mokainos <72137340+mokainos@users.noreply.github.com>
2021-05-17 14:02:22 +0100

    modified example

--- example/example.tf
+++ example/example.tf
@@ -26,16 +26,21 @@ provider "azurerm" {
   alias           = "privatezones"
 }
 
+module "acmetags" {
+  source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
+  environment = "sbox"
+  product     = "acme"
+  builtFrom   = "terraform-module-acme-function"
+}
+
 module "acme" {
   source = "git::https://github.com/hmcts/terraform-module-acme-function.git?ref=master"
   providers = {
     azurerm.dnszone     = azurerm.dnszones
     azurerm.privatezone = azurerm.privatezones
   }
-  location  = "uk south"
-  env       = "sbox"
-  dns_zones = ["sandbox.platform.hmcts.net"]
-  common_tags = {
-    app = "acme"
-  }
+  location    = "uk south"
+  env         = "sbox"
+  dns_zones   = ["sandbox.platform.hmcts.net"]
+  common_tags = module.acmetags.common_tags
 }
