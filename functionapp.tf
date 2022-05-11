b27d1fbb45421a46d09f5b83816e046be2665cb7
Enda Kelly <enda.kelly@hmcts.net>
2022-05-11 11:47:48 +0100

    change storage account replication type to variable

--- functionapp.tf
+++ functionapp.tf
@@ -36,7 +36,7 @@ resource "azurerm_storage_account" "stg" {
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.location
   account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_replication_type = var.storage_account_repl_type
   tags                     = var.common_tags
 }
 
