c57a3da44430b04080dac74b6571849f5a8fc6e6
Enda Kelly <enda.kelly@hmcts.net>
2022-05-11 12:38:27 +0100

    rename variable

--- variables.tf
+++ variables.tf
@@ -28,6 +28,6 @@ variable "dns_contributor_group_id" {
   description = "DTS Public DNS Contributor group"
 }
 
-variable "storage_account_repl_type" {
+variable "acme_storage_account_repl_type" {
   description = "Replication type for acme storage account"
 }
