f49334421f07f313d61f2b8476f5803dccec0487
Enda Kelly <enda.kelly@hmcts.net>
2022-09-06 12:12:03 +0100

    update data resource

--- data.tf
+++ data.tf
@@ -1,13 +1,13 @@
 data "azurerm_client_config" "current" {
 
 }
-data "azurerm_subscription" "dns_zone" {
-  provider = azurerm.dnszone
-}
+data "azurerm_subscription" "dns_zone" {}
 
 data "azuread_group" "group" {
   display_name     = "DTS Platform Operations"
   security_enabled = true
 }
 
-data "azurerm_subscription" "subscriptionid" {}
+data "azurerm_subscription" "subscriptionid" {
+  subscription_id = var.subscription_id
+}
