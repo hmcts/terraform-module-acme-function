ddb5541e855493aafeaf47ba6b343d16520631da
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2022-10-03 14:36:02 +0100

    add oauth2 settings to app reg

--- local.tf
+++ local.tf
@@ -1,4 +1,4 @@
 locals {
   name = replace(lower(data.azurerm_subscription.subscriptionid.display_name), "sharedservices", "sds")
-
+  app_name = "acme-${lower(data.azurerm_subscription.subscriptionid.display_name)}"
 }
