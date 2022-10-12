029b43e9a06625418cd0d4db122d34a03f343297
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2022-10-12 10:07:18 +0100

    undo previous

--- data.tf
+++ data.tf
@@ -3,6 +3,15 @@ data "azurerm_client_config" "current" {
 }
 data "azurerm_subscription" "dns_zone" {}
 
+data "azuread_group" "group" {
+  display_name     = "DTS Platform Operations"
+  security_enabled = true
+}
+
 data "azurerm_subscription" "subscriptionid" {
   subscription_id = var.subscription_id
+}
+
+data "azuread_group" "dns_contributor" {
+  display_name = "DTS Public DNS Contributor (env:${lower(var.env)})"
 }
