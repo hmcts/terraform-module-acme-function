ec63b1b129e1cca5c5d0facb1a89edc1499089c1
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2022-10-21 11:49:42 +0100

    update variable

--- variables.tf
+++ variables.tf
@@ -36,6 +36,6 @@ variable "allow_nested_items_to_be_public" {
   default = false
 }
 
-variable "platform_operations_group" {}
+variable "administrator_ad_group_object_id" {}
 
 variable "dns_contributor_group_object_id" {}
