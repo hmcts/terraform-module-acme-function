ae3f26c152549f7166309cb2100b0c031343426d
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2022-10-21 10:27:09 +0100

    remove commented code

--- functionapp.tf
+++ functionapp.tf
@@ -49,7 +49,6 @@ resource "azurerm_windows_function_app" "funcapp" {
     unauthenticated_client_action = "RedirectToLoginPage"
     default_provider              = "AzureActiveDirectory"
     active_directory {
-      # client_id = azuread_application.appreg.application_id
       client_id = var.application_id
     }
   }
@@ -67,40 +66,6 @@ resource "azurerm_windows_function_app" "funcapp" {
   }
   tags = var.common_tags
 }
-
-# resource "random_uuid" "app_uuid" {}
-
-# resource "azuread_application" "appreg" {
-#   display_name = local.app_name
-#   api {
-#     oauth2_permission_scope {
-#       admin_consent_description  = "Allow the application to access ${local.app_name} on behalf of the signed-in user."
-#       admin_consent_display_name = "Access ${local.app_name}"
-#       id                         = random_uuid.app_uuid.result
-#       enabled                    = true
-#       type                       = "User"
-#       user_consent_description   = "Allow the application to access ${local.app_name} on your behalf."
-#       user_consent_display_name  = "Access ${local.app_name}"
-#       value                      = "user_impersonation"
-#     }
-#   }
-#   web {
-#     redirect_uris = ["https://acme${replace(local.name, "-", "")}.azurewebsites.net/.auth/login/aad/callback"]
-
-#     implicit_grant {
-#       access_token_issuance_enabled = true
-#       id_token_issuance_enabled     = true
-#     }
-#   }
-#   required_resource_access {
-#     resource_app_id = "00000003-0000-0000-c000-000000000000"
-
-#     resource_access {
-#       id   = "e1fe6dd8-ba31-4d61-89e7-88639da4683d"
-#       type = "Scope"
-#     }
-#   }
-# }
 
 resource "azuread_group_member" "dnszonecontributor" {
   group_object_id  = var.dns_contributor_group
