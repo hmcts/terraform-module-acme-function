0ac20bc29f4f9eb31246c7425742b1b98a210508
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2022-10-21 11:50:49 +0100

    update variable

--- functionapp.tf
+++ functionapp.tf
@@ -68,6 +68,6 @@ resource "azurerm_windows_function_app" "funcapp" {
 }
 
 resource "azuread_group_member" "dnszonecontributor" {
-  group_object_id  = var.dns_contributor_group
+  group_object_id  = var.dns_contributor_group_object_id
   member_object_id = azurerm_windows_function_app.funcapp.identity[0].principal_id
 }
